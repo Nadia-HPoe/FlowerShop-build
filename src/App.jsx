8c3f198379851cd8d94a9484a9c472d324217bf1
Nadia-HPoe <isslandia@mail.ru>
2023-11-29 12:52:55 +0300

    creating classnames and positioning for header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar } from './components/layout/Navbar';
 import { Footer } from './pages/Footer';
 import { Home } from './pages/Main';
+import './assets/styles/global.scss';
 
 function App() {
   return (
--- src/components/layout/Navbar/index.jsx
+++ src/components/layout/Navbar/index.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import './navbar.scss';
 
 function Navbar() {
   return (
-    <header>
-      <p>Flower Shop</p>
-      <a>Home</a>
-      <a>Shop</a>
-      <a>Blog</a>
-      <a>About</a>
-      <button>L</button>
-      <button>C</button>
+    <header className="header">
+      <div className="header__wrapper">
+        <a href="/" className="header__logo">
+          Flower Shop
+        </a>
+
+        <nav className="header__menu">
+          <ul className="header__list">
+            <li className="header__list-item">
+              <a href="/" className="header__link">
+                Home
+              </a>
+            </li>
+            <li className="header__list-item">
+              <a href="/" className="header__link">
+                Shop
+              </a>
+            </li>
+            <li className="header__list-item">
+              <a href="/" className="header__link">
+                Blog
+              </a>
+            </li>
+            <li className="header__list-item">
+              <a href="/" className="header__link">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="header__button">
+          <button className="header__login">L</button>
+          <button className="header__card">C</button>
+        </div>
+      </div>
     </header>
   );
 }
