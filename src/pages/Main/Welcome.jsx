ef37c331406dac1c70edb19cf92a2b669c40012e
Nadia-HPoe <isslandia@mail.ru>
2023-12-01 17:14:57 +0300

    adding pictures

---
+++ src/pages/Main/Welcome.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Image1 from '../../assets/images/fsh_flpic1.jpg';
+import Image2 from '../../assets/images/fsh_flpic2.jpg';
+import Image3 from '../../assets/images/fsh_flpic3.jpg';
+import Image4 from '../../assets/images/fsh_flpic4.jpg';
+import Image5 from '../../assets/images/fsh_flpic5.jpg';
+import Image6 from '../../assets/images/fsh_flpic6.jpg';
+
+import './main.scss';
+
+function Welcome() {
+  return (
+    <section className="welcome-screen">
+      <div className="welcome__wrapper">
+        <div className="welcome">
+          <h1 className="welcome__title">
+            Flowers, 🌻 <br /> that the world <br /> needs
+          </h1>
+          <p className="welcome__text">Browse through hundreds of flowers</p>
+          <button className="welcome__button">Browse</button>
+        </div>
+
+        <div className="welcome__gallery">
+          <img
+            src={Image1}
+            alt="flower"
+            className="welcome__gallery-item img-fluid"
+          />
+          <img
+            src={Image2}
+            alt="flower"
+            className="welcome__gallery-item img-fluid"
+          />
+          <img
+            src={Image3}
+            alt="flower"
+            className="welcome__gallery-item img-fluid"
+          />
+          <img
+            src={Image4}
+            alt="flower"
+            className="welcome__gallery-item img-fluid"
+          />
+          <img
+            src={Image5}
+            alt="flower"
+            className="welcome__gallery-item img-fluid"
+          />
+          <img
+            src={Image6}
+            alt="flower"
+            className="welcome__gallery-item img-fluid"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { Welcome };
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,38 +1,18 @@
 import React from 'react';
-import Image1 from '../../assets/images/fsh_flpic1.jpg';
-import './main.scss';
+import { Welcome } from './Welcome';
 
 function Home() {
   return (
     <main className="homepage">
-      <section className="welcome-screen">
-        <div className="container">
-          <div className="welcome__wrapper">
-            <div className="welcome">
-              <h1 className="welcome__title">
-                Flowers, 🌻 that the world needs
-              </h1>
-              <p className="welcome__text">
-                Browse between hundreds of flowers
-              </p>
-              <button className="welcome__button">Browse</button>
-            </div>
-
-            <div className="welcome__gallery">
-              <img src={Image1} alt="flower" />
-              <img src={Image1} alt="flower" />
-              <img src={Image1} alt="flower" />
-              <img src={Image1} alt="flower" />
-              <img src={Image1} alt="flower" />
-              <img src={Image1} alt="flower" />
-            </div>
-          </div>
-        </div>
-      </section>
+      <Welcome />
 
       <section className="bestsellers">
         <h4>Bestsellers</h4>
-        <img src="" alt="flower" />
+        <div className="bestsellers-card">
+          <img src="" alt="flower" />
+          <p>Daisy</p>
+        </div>
+
         <img src="" alt="flower" />
         <img src="" alt="flower" />
         <img src="" alt="flower" />
